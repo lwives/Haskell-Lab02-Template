53b53df9fab252cae4e6d9405dcd5f7610ce085a
Leandro Krug Wives <lwives@users.noreply.github.com>
2025-09-09 15:30:34 -0300

    Refactor tests to use assertApproxEqual

--- Exercicio-07/TestExercicio07.hs
+++ Exercicio-07/TestExercicio07.hs
@@ -1,19 +1,20 @@
 module Main where
 
 import Test.HUnit
-import Test.HUnit.Approx ( (~?=~) )
+import Test.HUnit.Approx ( assertApproxEqual )
 import Exercicio07 (similaridade)
 
 testSimilaridade :: Test
 testSimilaridade = TestList [
-    -- Exemplo do enunciado
-    "teste 1: vetores de exemplo" ~: similaridade [0.1, 0.4, 0.9] [0.2, 0.4, 0.5] ~?=~ 0.63 / (0.9899494936611666 * 0.6708203932499369),
-
-    -- Casos de teste adicionais
-    "teste 2: vetores identicos" ~: similaridade [1.0, 1.0, 1.0] [1.0, 1.0, 1.0] ~?=~ 1.0,
-    "teste 3: vetores ortogonais (produto interno 0)" ~: similaridade [1.0, 0.0] [0.0, 1.0] ~?=~ 0.0,
-    "teste 4: um vetor vazio" ~: similaridade [1.0, 2.0] [] ~?=~ 0.0,
-    "teste 5: ambos os vetores vazios" ~: similaridade [] [] ~?=~ 0.0
+    -- O primeiro argumento de 'assertApproxEqual' eh a mensagem do teste
+    -- O segundo eh a tolerancia (um numero pequeno)
+    -- O terceiro eh o valor esperado
+    -- O quarto eh o valor obtido da sua funcao
+    "teste 1: vetores de exemplo" ~: assertApproxEqual "" 0.000001 (0.63 / (0.9899494936611666 * 0.6708203932499369)) (similaridade [0.1, 0.4, 0.9] [0.2, 0.4, 0.5]),
+    "teste 2: vetores identicos" ~: assertApproxEqual "" 0.000001 1.0 (similaridade [1.0, 1.0, 1.0] [1.0, 1.0, 1.0]),
+    "teste 3: vetores ortogonais (produto interno 0)" ~: assertApproxEqual "" 0.000001 0.0 (similaridade [1.0, 0.0] [0.0, 1.0]),
+    "teste 4: um vetor vazio" ~: assertApproxEqual "" 0.000001 0.0 (similaridade [1.0, 2.0] []),
+    "teste 5: ambos os vetores vazios" ~: assertApproxEqual "" 0.000001 0.0 (similaridade [] [])
     ]
 
 main :: IO ()
