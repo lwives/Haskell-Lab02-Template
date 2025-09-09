f69b4da0a77ef32304d6f1e03e7d302402668ad6
Leandro Krug Wives <lwives@users.noreply.github.com>
2025-09-09 14:57:04 -0300

    Fix similarity test case with updated values

--- Exercicio-07/TestExercicio07.hs
+++ Exercicio-07/TestExercicio07.hs
@@ -7,7 +7,7 @@ import Exercicio07 (similaridade)
 testSimilaridade :: Test
 testSimilaridade = TestList [
     -- Exemplo do enunciado
-    "teste 1: vetores de exemplo" ~: similaridade [0.1, 0.4, 0.9] [0.2, 0.4, 0.5] ~?=~ 0.63 / (0.547722557505 * 0.7348469228349534),
+    "teste 1: vetores de exemplo" ~: similaridade [0.1, 0.4, 0.9] [0.2, 0.4, 0.5] ~?=~ 0.63 / (0.9899494936611666 * 0.6708203932499369),
 
     -- Casos de teste adicionais
     "teste 2: vetores identicos" ~: similaridade [1.0, 1.0, 1.0] [1.0, 1.0, 1.0] ~?=~ 1.0,
