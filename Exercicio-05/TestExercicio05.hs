56f2aa2fb4139b7414b04a783996cac9f0a04f5f
Leandro Krug Wives <lwives@users.noreply.github.com>
2025-09-09 15:30:00 -0300

    Refactor tests to use assertApproxEqual

--- Exercicio-05/TestExercicio05.hs
+++ Exercicio-05/TestExercicio05.hs
@@ -1,17 +1,17 @@
 module Main where
 
 import Test.HUnit
-import Test.HUnit.Approx ( (~?=~) )
+import Test.HUnit.Approx ( assertApproxEqual )
 import Exercicio05 (norma)
 
 testNorma :: Test
 testNorma = TestList [
-    "teste 1: vetor com 4 elementos" ~: norma [0.1, 0.2, 0.3, 0.4] ~?=~ 0.547722557505,
-    "teste 2: vetor de um elemento" ~: norma [0.5] ~?=~ 0.5,
-    "teste 3: vetor com valores zero" ~: norma [0.0, 0.0, 0.0] ~?=~ 0.0,
-    "teste 4: vetor com um unico valor nao zero" ~: norma [0.0, 0.8, 0.0] ~?=~ 0.8,
-    "teste 5: vetor vazio" ~: norma [] ~?=~ 0.0
-    ]
+    "teste 1: vetor com 4 elementos" ~: assertApproxEqual "Verifica o vetor de exemplo" 0.000001 0.547722557505 (norma [0.1, 0.2, 0.3, 0.4]),
+    "teste 2: vetor de um elemento" ~: assertApproxEqual "Verifica um vetor unitario" 0.000001 0.5 (norma [0.5]),
+    "teste 3: vetor com valores zero" ~: assertApproxEqual "Verifica o vetor de zeros" 0.000001 0.0 (norma [0.0, 0.0, 0.0]),
+    "teste 4: vetor com um unico valor nao zero" ~: assertApproxEqual "Verifica um vetor com um so valor" 0.000001 0.8 (norma [0.0, 0.8, 0.0]),
+    "teste 5: vetor vazio" ~: assertApproxEqual "Verifica o vetor vazio" 0.000001 0.0 (norma [])
+    ]   
 
 main :: IO ()
 main = runTestTT testNorma >>= print
